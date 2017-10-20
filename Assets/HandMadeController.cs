95e0e6b9cb7cfd4496b10bd2f18554d40d645b29
Kudo <ku.kn8821da@gmail.com>
2017-10-20 16:51:26 +0900

    no message

---
+++ Assets/HandMadeController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandMadeController : MonoBehaviour
+{
+
+    Touch touched = new Touch();
+    [SerializeField] private RectTransform canvas;
+    Vector3 touchWorldPos;
+    Vector3 startPos;
+    int padRange = 70;
+    Vector3 padPos;
+    Vector3 adjustVec;
+    [SerializeField] private GameObject player;
+    int speed = 3;
+
+    // Use this for initialization
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+
+            touched = Input.GetTouch(0);
+            RectTransformUtility.ScreenPointToWorldPointInRectangle(canvas, touched.position, null, out touchWorldPos);
+
+            if (Vector3.Distance(startPos, touchWorldPos) <= padRange)
+            {
+                padPos = touchWorldPos;
+            }
+            else
+            {
+                adjustVec = (touchWorldPos - startPos).normalized * padRange;
+                padPos = startPos + adjustVec;
+            }
+
+            transform.position = padPos;
+            player.transform.Translate((padPos.x - startPos.x) / padRange * speed * Time.deltaTime, 0, (padPos.y - startPos.y) / padRange * speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = startPos;
+        }
+    }
+}
